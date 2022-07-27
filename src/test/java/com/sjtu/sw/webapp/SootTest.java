44e23a2a0e36d9c92f86a8948bb535e994c97efe
wshi8 <wshi8@trip.com>
2022-07-27 18:31:15 +0800

    solve class path

--- src/test/java/com/sjtu/sw/webapp/SootTest.java
+++ src/test/java/com/sjtu/sw/webapp/SootTest.java
@@ -7,10 +7,21 @@ import soot.PackManager;
 import soot.Transform;
 import soot.toolkits.graph.ExceptionalUnitGraph;
 
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Map;
 
 public class SootTest {
     public static void main(String[] args) {
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+        URL[] urls = ((URLClassLoader) cl).getURLs();
+        StringBuilder sb = new StringBuilder();
+        for (URL url : urls) {
+            String jarpath = url.getFile().substring(1).replaceAll("%20", " ");
+            sb.append(jarpath).append(';');
+        }
+        String classPath = sb.toString();
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         PackManager.v().getPack("jtp").add(
             new Transform("jtp.myTransform", new BodyTransformer() {
                 @Override
@@ -19,10 +30,16 @@ public class SootTest {
                 }
             })
         );
-        soot.Main.main(new String[]{ "-v",
+//        soot.tools.CFGViewer.main(new String[]{ "-v", "-cp", classPath,
+//            "-process-dir",
+//            "target/webapp-0.0.1-SNAPSHOT/WEB-INF/classes",
+//            "-process-jar-dir", "target/webapp-0.0.1-SNAPSHOT/WEB-INF/lib",
+//            "com.sjtu.sw.webapp.controller.FileController"});
+        soot.Main.main(new String[]{ "-v", "-cp", classPath,
             "-process-dir",
-            "D:\\Dev\\workspace\\webapp\\temp\\targets",
-            "-process-jar-dir", "D:\\Dev\\workspace\\webapp\\target\\webapp-0.0.1-SNAPSHOT\\WEB-INF\\lib",
+            "target/webapp-0.0.1-SNAPSHOT/WEB-INF/classes",
+            "-process-jar-dir", "target/webapp-0.0.1-SNAPSHOT/WEB-INF/lib",
+            "-dump-cfg", "wjtp",
             "com.sjtu.sw.webapp.controller.FileController"});
     }
 }
